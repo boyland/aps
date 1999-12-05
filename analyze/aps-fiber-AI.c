54387abeea8d03d18d5a8931e6bb0506c6811459
yw <yw>
1999-12-05 19:22:51 +0000

    Initial revision

---
+++ analyze/aps-fiber-AI.c
@@ -0,0 +1,10 @@
+// Implementation for 790-2 (Program Analysis) project
+// Dec. 1999
+// Yu Wang
+
+
+void *count(void *accum, void *node) {
+    ++ *((int *)accum);
+    return accum;
+}
+
---
+++ analyze/aps-fiber-AI.h
@@ -0,0 +1,5 @@
+// declarations for 790-2 (Program Analysis) project
+// Dec. 1999
+// Yu Wang
+
+void *count(void *accum, void *node) ;
