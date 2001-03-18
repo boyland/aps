34e40084fdd1ca943d40fc3c42fa3e3a9a5f25e2
boyland <boyland>
2001-03-18 03:16:02 +0000

    Added type environments. Added TypeActual and Match sequence links.

--- analyze/aps-bind.h
+++ analyze/aps-bind.h
@@ -3,6 +3,21 @@ extern Declaration first_Declaration(Declarations);
 extern Expression first_Actual(Actuals);
 extern Expression first_Expression(Expressions);
 extern Pattern first_PatternActual(PatternActuals);
+extern Type first_TypeActual(TypeActuals);
+extern Match first_Match(Matches);
+
+struct TypeContour {
+  struct TypeContour *outer; /*  nested type environment */
+  Declaration source; /* module, class, polymorphic */
+  Declarations type_formals; /* numbered for polyrmorphic */
+  Declaration result; /* type declaration */
+  union {
+    TypeActuals type_actuals; // for module/class
+    Type* inferred; // array of Types (initialized to NULL in aps-bind)
+  } u;
+};
+  
+typedef struct TypeContour *TypeEnvironment;
 
 extern int bind_debug;
 #define PRAGMA_ACTIVATION 1
