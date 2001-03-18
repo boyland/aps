38b940674084f2f2011bb0195a9cd7d9fbb073af
boyland <boyland>
2001-03-18 03:11:02 +0000

    Added match routines that simplify top_level_match routines, and generalized both to allow lhd decl to be omitted.

--- analyze/aps-ag-util.c
+++ analyze/aps-ag-util.c
@@ -9,15 +9,24 @@
  */
 
 Declaration top_level_match_lhs_decl(Declaration tlm) {
-  Pattern pat=matcher_pat(top_level_match_m(tlm));
+  return match_lhs_decl(top_level_match_m(tlm));
+}
+
+Declaration match_lhs_decl(Match m) {
+  Pattern pat=matcher_pat(m);
   switch (Pattern_KEY(pat)) {
-  default: fatal_error("%d:improper top-level-match",
-		       tnode_line_number(tlm));
+  default: fatal_error("%d:improper match",
+		       tnode_line_number(m));
+  case KEYpattern_call:
+    return NULL;
+    break;
   case KEYand_pattern:
     pat = and_pattern_p1(pat);
     switch (Pattern_KEY(pat)) {
-    default: fatal_error("%d:improper top-level-match",
-			 tnode_line_number(tlm));
+    default:
+      fatal_error("%d:improper match",
+		  tnode_line_number(m));
+      return NULL;
     case KEYpattern_var:
       return pattern_var_formal(pat);
     }
@@ -25,48 +34,67 @@ Declaration top_level_match_lhs_decl(Declaration tlm) {
 }
 
 Declaration top_level_match_constructor_decl(Declaration tlm) {
-  Pattern pat=matcher_pat(top_level_match_m(tlm));
+  return match_constructor_decl(top_level_match_m(tlm));
+}
+
+Declaration match_constructor_decl(Match m) {
+  Pattern pat=matcher_pat(m);
   switch (Pattern_KEY(pat)) {
-  default: fatal_error("%d:improper top-level-match",
-		       tnode_line_number(tlm));
+  default: fatal_error("%d:improper match",
+		       tnode_line_number(m));
+  case KEYpattern_call:
+    break;
   case KEYand_pattern:
     pat = and_pattern_p2(pat);
-    switch (Pattern_KEY(pat)) {
-    default: fatal_error("%d:misformed pattern",tnode_line_number(pat));
-    case KEYpattern_call:
-      switch (Pattern_KEY(pattern_call_func(pat))) {
-      default:
-	fatal_error("%d:unknown pattern function",tnode_line_number(pat));
-      case KEYpattern_use:
-	{ Declaration decl =
-	    Use_info(pattern_use_use(pattern_call_func(pat)))->use_decl;
-	  if (decl == NULL) fatal_error("%d:unbound pfunc",
-					tnode_line_number(pat));
-	  return decl;
-	}
+    break;
+  }
+  
+  switch (Pattern_KEY(pat)) {
+  default: fatal_error("%d:misformed pattern",tnode_line_number(pat));
+  case KEYpattern_call:
+    switch (Pattern_KEY(pattern_call_func(pat))) {
+    default:
+      fatal_error("%d:unknown pattern function",tnode_line_number(pat));
+      return NULL;
+    case KEYpattern_use:
+      { Declaration decl =
+	  Use_info(pattern_use_use(pattern_call_func(pat)))->use_decl;
+      if (decl == NULL) fatal_error("%d:unbound pfunc",
+				    tnode_line_number(pat));
+      if (Declaration_KEY(decl) != KEYconstructor_decl)
+	fatal_error("%d: can only match on constructors directly",
+		    tnode_line_number(pat));
+      return decl;
       }
     }
   }
 }
 
 Declaration top_level_match_first_rhs_decl(Declaration tlm) {
-  Pattern pat=matcher_pat(top_level_match_m(tlm));
+  return match_first_rhs_decl(top_level_match_m(tlm));
+}
+
+Declaration match_first_rhs_decl(Match m) {
+  Pattern pat=matcher_pat(m);
   switch (Pattern_KEY(pat)) {
-  default: fatal_error("%d:improper top-level-match",
-		       tnode_line_number(tlm));
+  default: fatal_error("%d:improper match", tnode_line_number(m));
   case KEYand_pattern:
     pat = and_pattern_p2(pat);
+    break;
+  case KEYpattern_call:
+    break;
+  }
+  switch (Pattern_KEY(pat)) {
+  default: fatal_error("%d:misformed pattern",tnode_line_number(pat));
+  case KEYpattern_call:
+    pat = first_PatternActual(pattern_call_actuals(pat));
+    if (pat == NULL) return NULL;
     switch (Pattern_KEY(pat)) {
-    default: fatal_error("%d:misformed pattern",tnode_line_number(pat));
-    case KEYpattern_call:
-      pat = first_PatternActual(pattern_call_actuals(pat));
-      if (pat == NULL) return NULL;
-      switch (Pattern_KEY(pat)) {
-      default: fatal_error("%d:improper top-level-match",
-			   tnode_line_number(tlm));
-      case KEYpattern_var:
-	return pattern_var_formal(pat);
-      }
+    default:
+      fatal_error("%d:improper match", tnode_line_number(m));
+      return 0;
+    case KEYpattern_var:
+      return pattern_var_formal(pat);
     }
   }
 }
