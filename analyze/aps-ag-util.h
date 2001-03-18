a73dbc0b496a0336517bbc76eb54179d21f260a4
boyland <boyland>
2001-03-18 03:15:28 +0000

    Copied the tlm rules to be used for any matches.

--- analyze/aps-ag-util.h
+++ analyze/aps-ag-util.h
@@ -5,6 +5,9 @@
 extern Declaration top_level_match_lhs_decl(Declaration);
 extern Declaration top_level_match_constructor_decl(Declaration);
 extern Declaration top_level_match_first_rhs_decl(Declaration);
+extern Declaration match_lhs_decl(Match);
+extern Declaration match_constructor_decl(Match);
+extern Declaration match_first_rhs_decl(Match);
 extern Declaration next_rhs_decl(Declaration);
 
 
