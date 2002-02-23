8ff2c8717fc1577c1f08a56dc42a428c85dd5127
boyland <boyland>
2002-02-23 20:46:19 +0000

    Made analysis state available after analysis.

--- analyze/aps-analyze.h
+++ analyze/aps-analyze.h
@@ -1,1 +1,4 @@
-extern void analyze_Program(Program);
+#define MODULE_DECL_ANALYSIS_STATE(md) \
+	(STATE*)(Declaration_info(md)->analysis_state)
+
+extern void analyze_Program(Program); /* decorate modules with STATE */
