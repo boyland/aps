91df7f096d6254acccadafb63e340b400481d0d2
boyland <boyland>
2002-02-23 20:49:46 +0000

    Now generates conditional total order (not very well).

--- analyze/aps-oag.c
+++ analyze/aps-oag.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <jbb.h>
+#include "alloc.h"
 #include "aps-ag.h"
 
 int oag_debug;
@@ -114,8 +115,142 @@ void schedule_summary_dependency_graph(PHY_GRAPH *phy_graph) {
   }
 }
 
+CTO_NODE* schedule_rest(AUG_GRAPH *aug_graph,
+			CTO_NODE* prev,
+			CONDITION cond,
+			int remaining)
+{
+  CTO_NODE* cto_node = 0;
+  int i;
+  int n = aug_graph->instances.length;
+  int needed_condition_bits;
+
+  /* If nothing more to do, we are done. */
+  if (remaining == 0) return 0;
+
+  /* Pre-allocate a node */
+  cto_node = (CTO_NODE*)HALLOC(sizeof(CTO_NODE));
+  cto_node->cto_prev = prev;
+
+  for (i=0; i < n; ++i) {
+    INSTANCE *in1 = &aug_graph->instances.array[i];
+    int j;
+
+    /* If already scheduled, then ignore. */
+    if (aug_graph->schedule[i] != 0) continue;
+
+    needed_condition_bits = 0;
+
+    /* Look for a predecessor edge */
+    for (j=0; j < n; ++j) {
+      INSTANCE *in2 = &aug_graph->instances.array[j];
+      int index = j*n+i;
+      EDGESET edges;
+
+      /* Look at all dependencies from j to i */
+      for (edges = aug_graph->graph[index]; edges != 0; edges=edges->rest) {
+	CONDITION merged;
+	merged.positive = cond.positive | edges->cond.positive;
+	merged.negative = cond.negative | edges->cond.negative;
+
+	/* if the merge condition is impossible, ignore this edge */
+	if (merged.positive & merged.negative) continue;
+
+	if (oag_debug & PROD_ORDER_DEBUG) {
+	  int i=n-remaining;
+	  for (; i > 0; --i) printf("  ");
+	  if (aug_graph->schedule[j] == 0)
+	    printf("! ");
+	  else
+	    printf("? ");
+	  print_edge(edges,stdout);
+	}
+
+	/* If j not scheduled, then i cannot be considered */
+	if (aug_graph->schedule[j] == 0) break; /* leave edges != 0 */
+
+	/* see what conditions remain to be resolved. */
+	needed_condition_bits |= (edges->cond.positive&~cond.positive);
+	needed_condition_bits |= (edges->cond.negative&~cond.negative);
+      }
+
+      /* If a remaining edge, then i cannot be considered */
+      if (edges != 0) break;
+    }
+
+    /* If we got through all predecessors, we can stop */
+    if (j == n) break;
+  }
+
+  if (i == n) {
+    /* can't find anything */
+    int bits = cond.positive|cond.negative;
+    printf("Horrible hack (only works when conditions are ordered)\n");
+    needed_condition_bits = 1;
+    while (bits > needed_condition_bits)
+      needed_condition_bits <<= 1;
+  }
+
+  if (oag_debug & PROD_ORDER) {
+    int i=n-remaining;
+    for (; i > 0; --i) printf("  ");
+  }
+  if (needed_condition_bits) {
+    int cindex = 0;
+    int cmask = 1;
+
+    cto_node->cto_type = CTO_CONDITION;
+    /* find first needed condition bit */
+    while (!(needed_condition_bits & 1)) {
+      needed_condition_bits >>= 1;
+      ++cindex;
+      cmask <<= 1;
+    }
+    if (oag_debug & PROD_ORDER) {
+      printf("Condition %d\n",cindex);
+    }
+    cto_node->cto_cond_index = cindex;
+
+    cond.negative |= cmask;
+    cto_node->cto_cond_if_false =
+      schedule_rest(aug_graph,cto_node,cond,remaining);
+    cond.negative &= ~cmask;
+    cond.positive |= cmask;
+    cto_node->cto_cond_if_true =
+      schedule_rest(aug_graph,cto_node,cond,remaining);
+    cond.positive &= ~cmask;
+  } else {
+    INSTANCE* in = &aug_graph->instances.array[i];
+    if (oag_debug & PROD_ORDER) {
+      print_instance(in,stdout);
+      putchar('\n');
+    }
+    cto_node->cto_type = CTO_INSTANCE;
+    cto_node->cto_instance = in;
+    aug_graph->schedule[i] = 1;
+    cto_node->cto_next = schedule_rest(aug_graph,cto_node,cond,remaining-1);
+    aug_graph->schedule[i] = 0;
+  }
+  return cto_node;
+}
+
 void schedule_augmented_dependency_graph(AUG_GRAPH *aug_graph) {
+  int n = aug_graph->instances.length;
+  int i;
+  CONDITION cond;
+
   (void)close_augmented_dependency_graph(aug_graph);
+
+  /** Now schedule graph: we need to generate a conditional total order. */
+
+  /* we use the schedule array as temp storage */
+  for (i=0; i < n; ++i) {
+    aug_graph->schedule[i] = 0; /* This means: not scheduled yet */
+  }
+  cond.positive = 0;
+  cond.negative = 0;
+
+  aug_graph->total_order = schedule_rest(aug_graph,0,cond,n);
 }
 
 void compute_oag(Declaration module, STATE *s) {
