3e8e6b185e7b496f51db44f7d68f11312977a597
boyland <boyland>
2003-02-18 21:16:11 +0000

    new helper functions.

--- analyze/aps-ag-util.h
+++ analyze/aps-ag-util.h
@@ -3,9 +3,11 @@
  * and provide the following functions for accessing the pieces.
  */
 extern Declaration top_level_match_lhs_decl(Declaration);
+extern Use         top_level_match_constructor_use(Declaration);
 extern Declaration top_level_match_constructor_decl(Declaration);
 extern Declaration top_level_match_first_rhs_decl(Declaration);
 extern Declaration match_lhs_decl(Match);
+extern Use         match_constructor_use(Match);
 extern Declaration match_constructor_decl(Match);
 extern Declaration match_first_rhs_decl(Match);
 extern Declaration next_rhs_decl(Declaration);
