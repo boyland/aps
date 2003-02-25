d5f5bd46ae759ca54c65c3453a2c08de329492c6
boyland <boyland>
2003-02-25 05:13:10 +0000

    static scheduling. More options.

--- apscpp/apscpp.cc
+++ apscpp/apscpp.cc
@@ -10,38 +10,30 @@ extern "C" {
 #include <fstream>
 #include "dump-cpp.h"
 
-extern "C" 
-{
-  static int aps_error_count = 0;
-  
-  void aps_error(void *tnode, char *fmt, ...)
-{
-  va_list args;
-  va_start(args,fmt);
-  
-  (void)  fprintf(stderr, "%s.aps:%d:",
-                  aps_yyfilename,tnode_line_number(tnode));
-  (void) vfprintf(stderr, fmt, args);
-  (void)  fprintf(stderr, "\n");
-  (void)   fflush(stderr);
-  
-  va_end(args);
-  ++aps_error_count;
-}  
-}
+using namespace std;
 
-static void usage() {
-  fprintf(stderr,"apsc: usage: apsc.boot <file.aps>\n");
-  fprintf(stderr,"             compile APS to Lisp on stdout\n");
+extern "C" {
+
+int callset_AI(Declaration module, STATE *s) { return 0; }
+
+static char* argv0 = "apscpp";
+void usage() {
+  fprintf(stderr,"%s: usage: %s [-SV] [-D...] <file.aps>\n",argv0,argv0);
+  fprintf(stderr,"             compile APS to Java\n");
+  /*fprintf(stderr,"    -I    generate an incremental implementation\n");*/
+  fprintf(stderr,"    -S    generate a scheduled implementation\n");
+  fprintf(stderr,"    -D... turn on indicated debugging flags\n");
+  fprintf(stderr,"    -DH   list debugging flags\n");
+  fprintf(stderr,"    -V    increase verbosity of generation code\n");
   exit(1);
 }
 
-extern "C" {
 extern void init_lexer(FILE*);
 extern int aps_yyparse(void);
 }
 
-main(int argc,char **argv) {
+int main(int argc,char **argv) {
+  argv0 = argv[0];
   if (argc < 2) usage();
   for (int i=1; i < argc; ++i) {
     if (streq(argv[i],"--omit")) {
@@ -51,26 +43,37 @@ main(int argc,char **argv) {
       impl_module(argv[i+1],argv[i+2]);
       i += 2;
       continue;
-    } else if (streq(argv[i],"-Dt")) {
-      type_debug = 1;
+    } else if (streq(argv[i],"-I") || streq(argv[i],"--incremental")) {
+      incremental = true;
       continue;
+    } else if (streq(argv[i],"-S") || streq(argv[i],"--static")) {
+      static_schedule = true;
+      continue;
+    } else if (streq(argv[i],"-V") || streq(argv[i],"--verbose")) {
+      ++verbose;
+      continue;
+    } else if (argv[i][0] == '-' && argv[i][1] == 'D') {
+      set_debug_flags(argv[i]+2);
+      continue;
+    } else if (argv[i][0] == '-') {
+      usage();
     }
-    aps_error_count = 0;
     Program p = find_Program(make_string(argv[i]));
-    if (p == 0 || aps_error_count) {
-      fprintf(stderr,"compilation halted due to parse errors.\n");
+    if (p == 0) {
+      fprintf(stderr,"Cannot find APS compilation unit %s\n",argv[i]);
       continue;
     }
+    aps_check_error("parse");
     aps_yyfilename = (char*)program_name(p);
     bind_Program(p);
-    if (aps_error_count) {
-      aps_error(p,"compilation halted due to name errors");
-      continue;
-    }
+    aps_check_error("binding");
     type_Program(p);
-    if (aps_error_count) {
-      aps_error(p,"compilation halted due to type errors");
-      continue;
+    aps_check_error("type");
+    if (static_schedule) {
+      analyze_Program(p);
+      aps_check_error("analysis");
+      cerr << "Warning: static scheduling not implemented: reverting to dynamic..." << endl;
+      static_schedule = 0;
     }
     char* hfilename = str2cat(argv[i],".h");
     char* cppfilename = str2cat(argv[i],".cpp");
@@ -82,6 +85,7 @@ main(int argc,char **argv) {
   }
   exit(0);
 }
+
 
 
 
