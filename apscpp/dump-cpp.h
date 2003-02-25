36fd6c8de5229a145100ab9602d0cf48e987262e
boyland <boyland>
2003-02-25 05:20:04 +0000

    dump_cpp_Declarartion differnet, so removed. new options. std:: stuff

--- apscpp/dump-cpp.h
+++ apscpp/dump-cpp.h
@@ -1,8 +1,8 @@
 #ifndef DUMP_CPP_H
 #include <iostream>
 
-ostream& operator<<(ostream&,String);
-ostream& operator<<(ostream&,Symbol);
+std::ostream& operator<<(std::ostream&,String);
+std::ostream& operator<<(std::ostream&,Symbol);
 
 // don't generate any code for this declaration:
 void omit_declaration(char *name);
@@ -10,10 +10,13 @@ void omit_declaration(char *name);
 // The result type is as given:
 void impl_module(char *name, char *type);
 
+extern bool incremental;
+extern bool static_schedule;
+extern int verbose;
+
 void dump_cpp_Program(Program,std::ostream&,std::ostream&);
 void dump_cpp_Units(Units,std::ostream&,std::ostream&);
 void dump_cpp_Unit(Unit,std::ostream&,std::ostream&);
-void dump_cpp_Declaration(Declaration,Declaration context,std::ostream&,std::ostream&);
 
 struct ContextRecordNode {
   Declaration context;
@@ -23,14 +26,14 @@ struct ContextRecordNode {
     : context(d), extra(e), outer(o) {}
 };
 
-typedef void (*ASSIGNFUNC)(void *,Declaration,ostream&);
-void dump_Block(Block,ASSIGNFUNC,void*arg,ostream&);
-void dump_Type(Type,ostream&);
-void dump_Type_value(Type,ostream&);
-void dump_Typed_decl(Type,Declaration,char*prefix,ostream&);
-void dump_Expression(Expression,ostream&);
-void dump_Use(Use,char *prefix,ostream&);
-void dump_TypeEnvironments(Use,ostream&);
-void dump_Default(Default,ostream&);
+typedef void (*ASSIGNFUNC)(void *,Declaration,std::ostream&);
+void dump_Block(Block,ASSIGNFUNC,void*arg,std::ostream&);
+void dump_Type(Type,std::ostream&);
+void dump_Type_value(Type,std::ostream&);
+void dump_Typed_decl(Type,Declaration,char*prefix,std::ostream&);
+void dump_Expression(Expression,std::ostream&);
+void dump_Use(Use,char *prefix,std::ostream&);
+void dump_TypeEnvironment(TypeEnvironment,std::ostream&);
+void dump_Default(Default,std::ostream&);
 
 #endif
