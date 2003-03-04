e33e9dcd11171b7e9563def20b205de0bc646501
boyland <boyland>
2003-03-04 03:03:34 +0000

    Added extra overloadings to permit more to be done with <<.  Changed indent to be more general.

--- aps2scala/dump-scala.h
+++ aps2scala/dump-scala.h
@@ -1,8 +1,9 @@
 #ifndef DUMP_CPP_H
 #include <iostream>
+#include <string>
 
-std::ostream& operator<<(std::ostream&,String);
-std::ostream& operator<<(std::ostream&,Symbol);
+using std::ostream;
+using std::string;
 
 // don't generate any code for this declaration:
 void omit_declaration(char *name);
@@ -14,27 +15,98 @@ extern bool incremental;
 extern bool static_schedule;
 extern int verbose;
 
-void dump_cpp_Program(Program,std::ostream&,std::ostream&);
-void dump_cpp_Units(Units,std::ostream&,std::ostream&);
-void dump_cpp_Unit(Unit,std::ostream&,std::ostream&);
+class Implementation;
 
-struct ContextRecordNode {
+extern Implementation *impl;
+
+void dump_cpp_Program(Program,ostream&,ostream&);
+void dump_cpp_Units(Units,ostream&,ostream&);
+void dump_cpp_Unit(Unit,ostream&,ostream&);
+
+// If non-zero, this variable means that we
+// should do everything in oss.hs, not oss.cpps
+extern int inline_definitions;
+
+struct output_streams {
   Declaration context;
-  void *extra; /* branch in context (if any) */
-  ContextRecordNode* outer;
-  ContextRecordNode(Declaration d, void* e, ContextRecordNode* o)
-    : context(d), extra(e), outer(o) {}
+  ostream &hs, &cpps, &is;
+  string prefix;
+  output_streams(Declaration _c, ostream &_hs, ostream &_cpps, ostream &_is,
+		 string _p)
+    : context(_c), hs(_hs), cpps(_cpps), is(_is), prefix(_p) {}
 };
 
-typedef void (*ASSIGNFUNC)(void *,Declaration,std::ostream&);
-void dump_Block(Block,ASSIGNFUNC,void*arg,std::ostream&);
-void dump_Type(Type,std::ostream&);
-void dump_Type_value(Type,std::ostream&);
-void dump_Type_signature(Type,std::ostream&);
-void dump_Typed_decl(Type,Declaration,char*prefix,std::ostream&);
-void dump_Expression(Expression,std::ostream&);
-void dump_Use(Use,char *prefix,std::ostream&);
-void dump_TypeEnvironment(TypeEnvironment,std::ostream&);
-void dump_Default(Default,std::ostream&);
+void dump_cpp_Declaration(Declaration,const output_streams&);
+
+static const int indent_multiple = 2;
+extern int nesting_level;
+string indent();
+class InDefinition {
+  int saved_nesting;
+ public:
+  InDefinition(int nn=0) { saved_nesting = nesting_level; nesting_level = nn; }
+  ~InDefinition() { nesting_level = saved_nesting; }
+};
+
+#define INDEFINITION InDefinition xx(inline_definitions ? nesting_level : 0)
+
+void dump_Type_prefixed(Type,ostream&);
+void dump_Type(Type,ostream&);
+void dump_Type_value(Type,ostream&);
+void dump_Type_signature(Type,ostream&);
+void dump_Typed_decl(Type,Declaration,char*prefix,ostream&);
+void dump_Expression(Expression,ostream&);
+void dump_Use(Use,char *prefix,ostream&);
+void dump_TypeEnvironment(TypeEnvironment,ostream&);
+void dump_Default(Default,ostream&);
+
+// these two must always be called in pairs: the first
+// leaves information around for the second:
+void dump_Pattern_cond(Pattern p, string node, ostream&);
+void dump_Pattern_bindings(Pattern p, ostream&);
+
+// override <<
+ostream& operator<<(ostream&o,Symbol s);
+ostream& operator<<(ostream&o,String s);
+
+inline ostream& operator<<(ostream&o,Expression e)
+{
+  dump_Expression(e,o); 
+  return o;
+}
+
+inline ostream& operator<<(ostream&o,Type t)
+{
+  dump_Type(t,o);
+  return o;
+}
+
+inline ostream& operator<<(ostream& o, Default d) 
+{
+  dump_Default(d,o);
+  return o;
+}
+
+// wrappers
+struct as_sig {
+  Type type;
+  as_sig(Type t) : type(t) {}
+};
+struct as_val {
+  Type type;
+  as_val(Type t) : type(t) {}
+};
+
+inline ostream& operator<<(ostream& o, as_sig tas) 
+{
+  dump_Type_signature(tas.type,o);
+  return o;
+}
+
+inline ostream& operator<<(ostream& o, as_val tav)
+{
+  dump_Type_value(tav.type,o);
+  return o;
+}
 
 #endif
