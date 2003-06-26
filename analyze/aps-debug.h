8be3f84246002d959fe1063278ee7389377d194e
boyland <boyland>
2003-06-26 18:02:35 +0000

    ADded warnings.

--- analyze/aps-debug.h
+++ analyze/aps-debug.h
@@ -1,6 +1,7 @@
 #ifndef APS_DEBUG_H
 
 extern void aps_error(void *tnode, char *fmt, ...);
+extern void aps_warning(void *tnode, char *fmt, ...);
 extern void aps_check_error(char *type); // exit if errors due to type
 extern void set_debug_flags(char *options); // exit when -DH
 
