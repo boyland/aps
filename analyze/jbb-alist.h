33bfab409572a167dc23c4980674ccaac8698ebe
boyland <boyland>
2003-08-25 20:45:06 +0000

    Added alist accessors.

--- analyze/jbb-alist.h
+++ analyze/jbb-alist.h
@@ -5,3 +5,9 @@ extern void *rassoc(void *value, ALIST alist);
 
 /* modifies the entry, and returns OK, or else returns NOT_OK */
 extern BOOL setassoc(void *key, void *value, ALIST alist);
+
+/** accessors legal for non-null ALISTs */
+extern void* alist_key(ALIST);
+extern void* alist_value(ALIST);
+extern ALIST alist_next(ALIST);
+
