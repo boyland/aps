cc927a2587fce4743f5b9cb2da49070971590794
boyland <boyland>
2010-10-13 13:47:11 +0000

    Initial revision

---
+++ examples/scala/test-coll-driver.scala
@@ -0,0 +1,15 @@
+object TestCollDriver extends Application
+{
+    val m = M_TINY("Tiny");
+    type T_Tiny = m.T_Result;
+    val t_Tiny = m.t_Result;
+
+    val w = t_Tiny.v_branch(t_Tiny.v_leaf(3),t_Tiny.v_leaf(4));
+
+    Debug.activate();
+
+    m.finish();
+
+    println("sum is " + t_Tiny.v_sum);
+    println("leaves is " + t_Tiny.m_v_leaves);
+}
