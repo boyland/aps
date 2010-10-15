47757667401926e08bcb1fe20925111ca3e57481
boyland <boyland>
2010-10-15 20:03:47 +0000

    Initial revision

---
+++ examples/scala/test-cycle-driver.scala
@@ -0,0 +1,14 @@
+object TestCycleDriver extends Application
+{
+    val m = new M_TINY("Tiny");
+    type T_Tiny = m.T_Result;
+    val t_Tiny = m.t_Result;
+
+    val w = t_Tiny.v_branch(t_Tiny.v_leaf(3),t_Tiny.v_leaf(4));
+
+    Debug.activate();
+
+    m.finish();
+
+    println("leaves is " + t_Tiny.v_leaves);
+}
