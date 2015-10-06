8beea4fe4a58c07e40e116397df578f7d872d08b
boyland <boyland>
2015-10-06 19:27:19 +0000

    Next version.

--- aps2scala/version.h
+++ aps2scala/version.h
@@ -1,3 +1,5 @@
 // Undeclared version 0.1 used in 2010 with Scala 2.7 (and 2.8)
-// Version 0.2 used in 2015 with SCala 2.10 (and 2.11)
-#define VERSION "0.2"
+// Version 0.2 used in 2015 with Scala 2.10 (and 2.11)
+// Version 0.3 used in 2015 with better pattern matching.
+// Version 0.3.5 adds better control of collection attributes.
+#define VERSION "0.3.5"
