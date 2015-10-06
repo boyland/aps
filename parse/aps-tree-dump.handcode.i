e38c4e664f1be929edfa6219a46355647594a4c8
boyland <boyland>
2015-10-06 19:24:16 +0000

    Moved lisp dumping to library debug debugging.

--- parse/aps-tree-dump.handcode.i
+++ parse/aps-tree-dump.handcode.i
@@ -1,7 +1,49 @@
-extern void dump_lisp_Symbol(Symbol s);
+void dump_lisp_Symbol(Symbol s) {
+  char *name = symbol_name(s);
+  printf(" aps-boot::|");
+  while (*name != '\0') {
+    switch (*name) {
+    case '\\':
+    case '|':
+      putchar('\\');
+      putchar(*name);
+      break;
+    default:
+      putchar(*name);
+      break;
+    }
+    ++name;
+  }
+  putchar('|');
+}
 
-extern void dump_lisp_String(String s);
+void dump_lisp_String(String st) {
+  /* a disgusting kludge */
+  char *s = (char *)st;
+  char end = '\0';
+  /* fprintf(stderr,"Converting %s\n",s); */
+  printf(" \"");
+  if (*s== '"') { /* a string */
+    ++s;
+    end = '"';
+  }
+  for (; *s != end; ++s) {
+    if (s[0] == '\\') {
+      ++s;
+      if (s[0] == 'n') {
+	printf("\n");
+      } else {
+	printf("\\%c",*s);
+      }
+    } else if (*s == '"') {
+      printf("\\\"");
+    } else {
+      printf("%c",*s);
+    }
+  }
+  printf("\"");
+}
 
-extern void dump_lisp_Boolean(Boolean b);
-
-
+void dump_lisp_Boolean(Boolean b) {
+  if (b) printf(" t"); else printf(" nil");
+}
--- parse/apsc.c
+++ parse/apsc.c
@@ -10,56 +10,6 @@ static void usage() {
 Program the_tree = NULL;
 int aps_parse_error = 0;
 
-void dump_lisp_Symbol(Symbol s) {
-  char *name = symbol_name(s);
-  printf(" aps-boot::|");
-  while (*name != '\0') {
-    switch (*name) {
-    case '\\':
-    case '|':
-      putchar('\\');
-      putchar(*name);
-      break;
-    default:
-      putchar(*name);
-      break;
-    }
-    ++name;
-  }
-  putchar('|');
-}
-
-void dump_lisp_String(String st) {
-  /* a disgusting kludge */
-  char *s = (char *)st;
-  char end = '\0';
-  /* fprintf(stderr,"Converting %s\n",s); */
-  printf(" \"");
-  if (*s== '"') { /* a string */
-    ++s;
-    end = '"';
-  }
-  for (; *s != end; ++s) {
-    if (s[0] == '\\') {
-      ++s;
-      if (s[0] == 'n') {
-	printf("\n");
-      } else {
-	printf("\\%c",*s);
-      }
-    } else if (*s == '"') {
-      printf("\\\"");
-    } else {
-      printf("%c",*s);
-    }
-  }
-  printf("\"");
-}
-
-void dump_lisp_Boolean(Boolean b) {
-  if (b) printf(" t"); else printf(" nil");
-}
-
 int info_size = 0;
 
 main(int argc,char **argv) {
