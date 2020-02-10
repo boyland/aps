ec7e09d703c97bb9b054014a9d5d305af685831e
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2020-02-10 15:15:56 -0600

    Fixed MIN MAX Lattice type mismatch issue (#29)
    * Fixed MIN MAX Lattice type mismatch issue
    
    * Update basic.handcode.scala

--- base/scala/basic.handcode.scala
+++ base/scala/basic.handcode.scala
@@ -57,6 +57,10 @@ object basic_implicit {
   val t_AndLattice = new M_MAKE_LATTICE[T_Boolean]("AndLattice",t_Boolean,v_true,v_andc,v_revimplies,v_and,v_or);
   type T_AndLattice = T_Boolean;
 
+  type T_MAX_LATTICE[T] = T;
+  type T_MIN_LATTICE[T] = T;
+
+
   type T_BAG[T] = List[T];
   type T_LIST[T] = List[T];
   type T_SET[T] = scala.collection.immutable.Set[T];
@@ -484,6 +488,7 @@ class M_MAX_LATTICE[T_TO]
 				   new M__basic_13[ T_TO](t_TO).v_max,
 				   new M__basic_13[ T_TO](t_TO).v_min)
       with C_MAX_LATTICE[T_TO,T_TO] with C_ORDERED[T_TO]
+      with C_TYPE[T_TO]
 {
   val v_less = t_TO.v_less;
   val v_less_equal = t_TO.v_less_equal;
@@ -502,6 +507,7 @@ extends M_MAKE_LATTICE[T_T](name,t_T,v_max_element,
 			    new M__basic_13[ T_T](t_T).v_min,
 			    new M__basic_13[ T_T](t_T).v_max)
       with C_MIN_LATTICE[T_T,T_T] with C_ORDERED[T_T]
+      with C_TYPE[T_T]
 {
   val v_less = t_T.v_less;
   val v_less_equal = t_T.v_less_equal;
