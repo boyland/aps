cafa54ada6fd1d259e7c450f7e898814b0c4b8ec
Amir <hesamian@uwm.edu>
2020-02-15 02:41:08 -0600

    Working attempt to fix type problem

--- base/scala/basic.handcode.scala
+++ base/scala/basic.handcode.scala
@@ -1382,3 +1382,38 @@ class M__basic_24[T_Node <: Node](t_Node:C_PHYLUM[T_Node]) {
     v_x.asInstanceOf[Node].lineNumber;
 };
 
+class M_MAX_LATTICE_Numeric[T_TO <: T_Integer]
+(name : String, t_TO:C_ORDERED[T_TO] with C_NUMERIC[T_TO],v_min_element : T_TO)
+  extends M_MAX_LATTICE[T_TO](name, t_TO, v_min_element)
+  with C_NUMERIC[T_TO]    // <-- New trait thanks to <: T_Integer
+{
+  override def v_zero: T_TO = t_TO.v_zero
+  override def v_one: T_TO = t_TO.v_one
+
+  override val v_plus: (T_TO, T_TO) => T_TO = t_TO.v_plus
+  override val v_minus: (T_TO, T_TO) => T_TO = t_TO.v_minus
+  override val v_times: (T_TO, T_TO) => T_TO = t_TO.v_times
+  override val v_divide: (T_TO, T_TO) => T_TO= t_TO.v_divide
+  override val v_unary_plus: T_TO => T_TO= t_TO.v_unary_plus
+  override val v_unary_minus: T_TO => T_TO = t_TO.v_unary_minus
+  override val v_unary_times: T_TO => T_TO = t_TO.v_unary_times
+  override val v_unary_divide: T_TO => T_TO = t_TO.v_unary_divide
+}
+
+class M_MIN_LATTICE_Numeric[T_TO <: T_Integer]
+(name : String, t_TO:C_ORDERED[T_TO] with C_NUMERIC[T_TO],v_max_element : T_TO)
+  extends M_MIN_LATTICE[T_TO](name, t_TO, v_max_element)
+  with C_NUMERIC[T_TO]    // <-- New trait thanks to <: T_Integer
+{
+  override def v_zero: T_TO = t_TO.v_zero
+  override def v_one: T_TO = t_TO.v_one
+
+  override val v_plus: (T_TO, T_TO) => T_TO = t_TO.v_plus
+  override val v_minus: (T_TO, T_TO) => T_TO = t_TO.v_minus
+  override val v_times: (T_TO, T_TO) => T_TO = t_TO.v_times
+  override val v_divide: (T_TO, T_TO) => T_TO= t_TO.v_divide
+  override val v_unary_plus: T_TO => T_TO= t_TO.v_unary_plus
+  override val v_unary_minus: T_TO => T_TO = t_TO.v_unary_minus
+  override val v_unary_times: T_TO => T_TO = t_TO.v_unary_times
+  override val v_unary_divide: T_TO => T_TO = t_TO.v_unary_divide
+}
