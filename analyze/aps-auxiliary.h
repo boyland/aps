fd434afe886acb5f7b677614411ad7bf564e1298
Amir <hesamian@uwm.edu>
2020-05-31 02:23:31 -0500

    Minor restructuring

--- analyze/aps-auxiliary.h
+++ analyze/aps-auxiliary.h
@@ -1,16 +1,17 @@
 #ifndef APS_AUXILIARY_H
 #define APS_AUXILIARY_H
 
+
 /**
  * Mimicking sig_inst
  */
 typedef struct InferredSignature_t {
+    struct InferredSignature_t *next;
     Boolean is_input;
     Boolean is_var;
     Class _class;
     Type *actuals;
     int num_actuals;
-    struct InferredSignature_t *next;
 } * InferredSignature;
 
 InferredSignature create_inferred_sig(Boolean is_input, Boolean is_var, Class _class,
