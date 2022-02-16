81cd19622ec9dee4440a654366d49617d5edbae8
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2022-02-15 20:10:47 -0600

    Fixed some problems with makefile and scala driver

--- examples/scala/classic-driver.scala
+++ examples/scala/classic-driver.scala
@@ -2,6 +2,7 @@
 
 object Classic extends App {
   var simple_tree : M_SIMPLE = null;
+  var p: Any = _;
   if (args.length == 0) {
     simple_tree = new M_SIMPLE("Simple");
     val t_simple = simple_tree.t_Result;
@@ -12,7 +13,7 @@ object Classic extends App {
 	t_simple.v_assign_stmt(t_simple.v_variable("x"),
 			       t_simple.v_variable("y"));
     val ss = t_simple.v_xcons_stmts(t_simple.v_no_stmts(),s);
-    val p = t_simple.v_program(t_simple.v_block(ds,ss));
+    p = t_simple.v_program(t_simple.v_block(ds,ss));
   } else {
     val ss = new SimpleScanner(new java.io.FileReader(args(0)));
     val sp = new SimpleParser();
@@ -22,6 +23,7 @@ object Classic extends App {
       System.exit(1);
     }
     simple_tree = sp.getTree();
+    p = simple_tree.t_Program;
   }
   
   val m_simple = simple_tree;
@@ -33,7 +35,9 @@ object Classic extends App {
   m_simple.finish();
   m_binding.finish();
   println("Messages:");
-  for (m <- t_binding.v_prog_msgs(p)) {
+
+  var program: t_binding.T_Program = p.asInstanceOf[t_binding.T_Program];
+  for (m <- t_binding.v_prog_msgs(program)) {
     println(m);
   }
 }
