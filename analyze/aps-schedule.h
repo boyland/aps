2c447cab262a730fa6c5ce9caeedb0a436a78514
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2022-05-20 13:38:47 -0500

    APS schedule header

---
+++ analyze/aps-schedule.h
@@ -0,0 +1,11 @@
+#ifndef AG_SCHEDULE_H
+#define AG_SCHEDULE_H
+
+/**
+ * @brief Computes total-preorder of set of attributes
+ * @param module APS module
+ * @param s state
+ */
+void compute_schedule(Declaration module, STATE* s);
+
+#endif
