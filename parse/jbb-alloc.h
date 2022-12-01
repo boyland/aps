46504bb3abaf7e88f498ca2f9cc2cdf03c36902b
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2022-12-01 01:38:09 -0600

    Removed unnessary change

--- parse/jbb-alloc.h
+++ parse/jbb-alloc.h
@@ -1,18 +1,12 @@
 #ifndef JBB_ALLOC_H
 #define JBB_ALLOC_H
 
-#ifdef __GNUC__
-#define VARIABLE_IS_NOT_USED __attribute__ ((unused))
-#else
-#define VARIABLE_IS_NOT_USED
-#endif
-
 extern void *heap, *heaptop, *more_heap(int);
 extern void *stack, *stacktop, *more_stack(int);
 extern void release(void *); /* release things from stack */
 
-static int VARIABLE_IS_NOT_USED ntemp;
-static void* VARIABLE_IS_NOT_USED temp;
+static int ntemp;
+static void* temp;
 #define ALIGN(size) (((size)+7)&~7)
 #define ALLOC(n,p,top,more) \
   (((p=(char *)(temp=p)+ALIGN(ntemp=n))>top)?more(ntemp):temp)
