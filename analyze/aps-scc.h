9d06bf91714b0e123d631d800b7047c7120a8f34
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2022-12-07 02:37:57 -0600

    updated a comment

--- analyze/aps-scc.h
+++ analyze/aps-scc.h
@@ -4,13 +4,13 @@
 #include "aps-dnc.h"
 
 /**
- * @brief sets phylum graph SCC components
+ * @brief sets phylum graph SCC components of instances
  * @param phy_graph phylum graph
  */
 void set_phylum_graph_components(PHY_GRAPH* phy_graph);
 
 /**
- * @brief sets augmented dependency graph SCC components
+ * @brief sets augmented dependency graph SCC components of instances
  * @param aug_graph augmented dependency graph
  */
 void set_aug_graph_components(AUG_GRAPH* aug_graph);
