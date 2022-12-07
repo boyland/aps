2e8b88e883267c3576ce6a6e4562cef70da797fa
Amir <hesamian@uwm.edu>
2022-12-07 02:17:11 -0600

    included new header in aps-ag

--- analyze/aps-scc.h
+++ analyze/aps-scc.h
@@ -1,8 +1,18 @@
 #ifndef APS_SCC_H
 #define APS_SCC_H
 
+#include "aps-dnc.h"
+
+/**
+ * @brief sets phylum graph SCC components
+ * @param phy_graph phylum graph
+ */
 void set_phylum_graph_components(PHY_GRAPH* phy_graph);
 
+/**
+ * @brief sets augmented dependency graph SCC components
+ * @param aug_graph augmented dependency graph
+ */
 void set_aug_graph_components(AUG_GRAPH* aug_graph);
 
 #endif
