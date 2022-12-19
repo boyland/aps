2f2c3bac746aa8c03db46963cce79b1200f72da9
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2022-12-19 14:26:29 -0600

    Grammar parser (#73)
    * Fixed some problems with makefile and scala driver
    
    * parser depends on parser base
    
    * some fixes
    
    * Update Makefile
    
    * Update Makefile
    
    * Added gramamr parser
    
    * Update Makefile
    
    * Update GrammarParserBase.scala
    
    * Update grammar-driver.scala
    
    * Minor renaming
    
    * some fix ups

---
+++ examples/scala/Grammar.y
@@ -0,0 +1,76 @@
+/* Complete parser for grammar.y */
+
+/*
+ * This fragment is intended to be used either by bison or ScalaBison,
+ * but will need language specific parts to compile as C/C++ or Scala
+ */
+
+%token <Symbol> TERMINAL NONTERMINAL
+%token SEMICOLON PIPE COLON
+
+%type <Grammar> grammar
+%type <Items> items
+%type <Item> item
+%type <ItemsMany> items_many
+%type <Productions> productions production_alt
+%%
+
+grammar : productions
+     { $$ = grammar($1); }
+        ;
+
+productions : /* NOTHING */
+   { $$ = productions_none(); }
+      | production_alt SEMICOLON productions
+   { $$ = productions_append($1, $3); }
+      ;
+
+production_alt : NONTERMINAL COLON items_many
+   { $$ = prods($1, $3); }
+      ;
+
+items_many : items
+  { $$ = items_many_single($1); }
+    | items_many PIPE items
+  { $$ = items_many_append($1, items_many_single($3)); }
+    ;
+
+items :  /* NOTHING */
+   { $$ = items_none(); }
+      | item items
+   { $$ = items_append(items_single($1), $2); }
+      ;
+
+item : TERMINAL
+  { $$ = terminal($1); }
+     | NONTERMINAL
+  { $$ = nonterminal($1); }
+     ;
+
+%%
+
+var scanner : GrammarScanner = null;
+var filename : String = "<unknown>";
+var result : Grammar = null;
+
+def get_result() : Grammar = result;
+
+def reset(sc : GrammarScanner, fn : String) : Unit = {
+  filename = fn;
+  scanner = sc;
+  yyreset(sc)
+};
+
+override def get_line_number() : Int = scanner.getLineNumber();
+
+/* This function is called automatically when Bison detects a parse error. */
+def yyerror(message : String) : Unit = {
+  print (filename + ":" + scanner.getLineNumber() + ": " +
+	     message + ", at or near " + yycur + "\n");
+};
+
+override def grammar(b:Productions) : Grammar = {
+  var res = super.grammar(b);
+  result = res;
+  res
+}
--- examples/scala/grammar-driver.scala
+++ examples/scala/grammar-driver.scala
@@ -1,51 +1,28 @@
 object GrammarDriver extends App {
-  Debug.activate();
 
   val symb = new M_SYMBOL("Symbol")
   symb.finish()
 
-  val grammar = new M_GRAMMAR("Grammar")
-  // Z -> Y X
-  // Y -> y
-  // X -> x
-  // X -> Z z
-  val t_Y = grammar.v_nonterminal(symb.v_create("Y"))
-  val t_X = grammar.v_nonterminal(symb.v_create("X"))
-  val t_Z = grammar.v_nonterminal(symb.v_create("Z"))
+  val ss = new GrammarScanner(new java.io.FileReader(args(0)));
+  val sp = new GrammarParser();
+  sp.reset(ss, args(0));
+  if (!sp.yyparse()) {
+    println("Errors found.\n");
+    System.exit(1);
+  }
+  var grammar_tree = sp.getTree();
+  val p = grammar_tree.t_Grammar;
+  
+  val m_grammar = grammar_tree;
+  val m_first = new M_FIRST[m_grammar.T_Result]("First", m_grammar.t_Result);
+  val m_follow = new M_FOLLOW[m_grammar.T_Result]("Follow", m_grammar.t_Result);
+  val m_nullable = new M_NULLABLE[m_grammar.T_Result]("Nullable", m_grammar.t_Result);
 
-  val t_y = grammar.v_terminal(symb.v_create("y"))
-  val t_x = grammar.v_terminal(symb.v_create("x"))
-  val t_z = grammar.v_terminal(symb.v_create("z"))
+  Debug.activate();
 
-  val prod1 = grammar.v_prod(
-    symb.v_create("Z"),
-    grammar.t_Items.v_append(grammar.t_Items.v_single(t_Y), grammar.t_Items.v_single(t_X))
-  )
-  val prod2 = grammar.v_prod(symb.v_create("Y"), grammar.t_Items.v_single(t_y))
-  val prod3 = grammar.v_prod(symb.v_create("X"), grammar.t_Items.v_single(t_x))
-  val prod4 = grammar.v_prod(
-    symb.v_create("X"),
-    grammar.t_Items.v_append(grammar.t_Items.v_single(t_Z), grammar.t_Items.v_single(t_z))
-  )
+  m_grammar.finish();
+  m_first.finish();
+  m_follow.finish();
+  m_nullable.finish();
 
-  val program = grammar.t_Productions.v_append(
-    grammar.t_Productions.v_append(
-      grammar.t_Productions.v_single(prod1),
-      grammar.t_Productions.v_single(prod2)
-    ),
-    grammar.t_Productions.v_append(
-      grammar.t_Productions.v_single(prod3),
-      grammar.t_Productions.v_single(prod4)
-    )
-  )
-
-  val root = grammar.v_grammar(program)
-
-  grammar.finish()
-
-  val first = new M_FIRST("First", grammar)
-  val follow = new M_FOLLOW("Follow", grammar)
-
-  first.finish()
-  follow.finish()
 }
