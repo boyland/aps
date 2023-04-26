06fbb42ba13cbd27b881b6b1f67108e6b25a8113
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2023-04-25 20:50:52 -0500

    Update basic.handcode.scala

--- base/scala/basic.handcode.scala
+++ base/scala/basic.handcode.scala
@@ -12,9 +12,9 @@ object basic_implicit {
   val v_true:T_Boolean = true;
   val v_false:T_Boolean = false;
   val v_and = f_and _;
-  def f_and(v__23: => T_Boolean, v__24: => T_Boolean): T_Boolean = v__23 && v__24;
+  def f_and(v__23: => T_Boolean, v__24: => T_Boolean):T_Boolean = v__23 && v__24;
   val v_or = f_or _;
-  def f_or(v__25: => T_Boolean, v__26: => T_Boolean): T_Boolean = v__25 || v__26;
+  def f_or(v__25: => T_Boolean, v__26: => T_Boolean):T_Boolean = v__25 || v__26;
   val v_not = f_not _;
   def f_not(v__27 : T_Boolean):T_Boolean = !v__27;
 
