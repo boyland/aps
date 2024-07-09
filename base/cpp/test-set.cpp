d2522890ad39294661130ed462db54e27ace20ee
John Boyland <boyland@uwm.edu>
2024-07-09 11:15:51 -0500

    Added equals tests

--- base/cpp/test-set.cpp
+++ base/cpp/test-set.cpp
@@ -23,15 +23,17 @@ static void assert_equals(const string &test, T expected, T actual)
 int main(int argc, char **argv)
 {
   if (argc > 1) verbose = true;
-  T_IntegerSet is, is2;
+  T_IntegerSet is, is2, is3;
   C_IntegerSet* t_IntegerSet = new C_IntegerSet(get_Integer());
   COLL<C_IntegerSet,C_Integer> iscoll(t_IntegerSet,get_Integer());
   C_IntegerSet& t = *t_IntegerSet;
   is = t.v_append(t.v_append(t.v_single(1),t.v_single(2)),t.v_single(3));
   is2 = t.v_append(t.v_append(t.v_single(3),t.v_single(0)),t.v_single(2));
+  is3 = t.v_append(t.v_append(t.v_single(3),t.v_single(1)),t.v_single(2));
 
   assert_equals("s1",string("{1,2,3}"),iscoll.to_string(is));
   assert_equals("s2",string("{3,0,2}"),iscoll.to_string(is2));
+  assert_equals("s3",string("{3,1,2}"),iscoll.to_string(is3));
 
   assert_equals("union(s1,s2)",string("{1,3,0,2}"),
 		iscoll.to_string(t.v_union(is,is2)));
@@ -40,5 +42,9 @@ int main(int argc, char **argv)
   assert_equals("difference(s1,s2)",string("{1}"),
 		iscoll.to_string(t.v_difference(is,is2)));
 
+  assert_equals("equal(s1,s2)",false,t.v_equal(is,is2));
+  assert_equals("equal(s1,s3)",true,t.v_equal(is,is3));
+  assert_equals("equal(s2,s3)",false,t.v_equal(is2,is3));
+  
   return failed;
 }
