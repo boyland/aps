96a6ed5f4d953973ba8c1b1b94e77f6cd308017e
John Boyland <boyland@Uwm.edu>
2024-07-08 21:07:51 -0500

    Made test-set self-checking.

--- base/cpp/test-set.cpp
+++ base/cpp/test-set.cpp
@@ -7,17 +7,38 @@ using namespace std;
 typedef C_SET<C_INTEGER> C_IntegerSet;
 typedef C_IntegerSet::T_Result T_IntegerSet;
 
-main()
+static bool verbose = false;
+static int failed = 0;
+
+template <class T>
+static void assert_equals(const string &test, T expected, T actual)
 {
+  bool passed = actual == expected;
+  if (verbose || !passed) cout << test << " = " << actual << endl;
+  if (passed) return;
+  ++failed;
+  cout << "  failed!  (Expected " << expected << ")" << endl;
+}
+
+int main(int argc, char **argv)
+{
+  if (argc > 1) verbose = true;
   T_IntegerSet is, is2;
   C_IntegerSet* t_IntegerSet = new C_IntegerSet(get_Integer());
   COLL<C_IntegerSet,C_Integer> iscoll(t_IntegerSet,get_Integer());
   C_IntegerSet& t = *t_IntegerSet;
   is = t.v_append(t.v_append(t.v_single(1),t.v_single(2)),t.v_single(3));
   is2 = t.v_append(t.v_append(t.v_single(3),t.v_single(0)),t.v_single(2));
-  cout << iscoll.to_string(is) << " with "
-       << iscoll.to_string(is2) << endl;
-  cout << "Union = " << iscoll.to_string(t.v_union(is,is2)) << endl;
-  cout << "Intersection = " << iscoll.to_string(t.v_intersect(is,is2)) << endl;
-  cout << "Difference = " << iscoll.to_string(t.v_difference(is,is2)) << endl;
+
+  assert_equals("s1",string("{1,2,3}"),iscoll.to_string(is));
+  assert_equals("s2",string("{3,0,2}"),iscoll.to_string(is2));
+
+  assert_equals("union(s1,s2)",string("{1,3,0,2}"),
+		iscoll.to_string(t.v_union(is,is2)));
+  assert_equals("intersect(s1,s2)",string("{2,3}"),
+		iscoll.to_string(t.v_intersect(is,is2)));
+  assert_equals("difference(s1,s2)",string("{1}"),
+		iscoll.to_string(t.v_difference(is,is2)));
+
+  return failed;
 }
